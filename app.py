a64036abaf8479d120500312e754074d2d465d67
Mark Neumann <markng@allenai.org>
2018-08-06 16:53:23 -0700

    more wrong imports

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ import psycopg2
 import pytz
 
 from allennlp.common.util import JsonDict, peak_memory_mb
-from allennlp.service.permalinks import int_to_slug, slug_to_int
-from allennlp.service.predictors import Predictor
+from allennlp.predictors import Predictor
 
+from server.permalinks import int_to_slug, slug_to_int
 from server.db import DemoDatabase, PostgresDemoDatabase
 from server.models import MODELS, DemoModel
 
