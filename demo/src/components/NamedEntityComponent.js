b066ad153038b1355e385c8ae0eb42e47db3ae3f
Mark Neumann <markng@allenai.org>
2018-08-31 15:37:35 -0700

    styling

--- demo/src/components/NamedEntityComponent.js
+++ demo/src/components/NamedEntityComponent.js
@@ -183,22 +183,68 @@ class TokenSpan extends React.Component {
         tooltip: "Person",
         color: "pink"
       },
-      "CARDINAL": {tooltip: "Cardinal Number", color: "orange"},
-      "DATE": {tooltip: "Date", color: "fuchsia"},
-      "FAC": {tooltip: "Fac", color: "cobalt"},
-      "GPE": {tooltip: "GPE", color: "teal"},
-      "LANGUAGE": {tooltip: "Language", color: "red"},
-      "LAW": {tooltip: "Law", color: "brown"},
+      "CARDINAL": {
+        tooltip: "Cardinal Number",
+        color: "orange"
+      },
+      "EVENT": {
+        tooltip: "Event",
+        color: "green"
+      },
+      "DATE": {
+        tooltip: "Date",
+        color: "fuchsia"
+      },
+      "FAC": {
+        tooltip: "Facility",
+        color: "cobalt"
+      },
+      "GPE": {
+        tooltip: "Country/City/State",
+        color: "teal"
+      },
+      "LANGUAGE": {
+        tooltip: "Language",
+        color: "red"
+      },
+      "LAW": {
+        tooltip: "Law",
+        color: "brown"
+      },
       // LOC - see above
-      "MONEY": {tooltip: "Monetary Value", color: "orange"},
-      "NORP": {tooltip: "Norp", color: "green"},
-      "ORDINAL": {tooltip: "ORDINAL", color: "orange"},
+      "MONEY": {
+        tooltip: "Monetary Value",
+        color: "orange"
+      },
+      "NORP": {
+        tooltip: "Nationalities, Religious/Political Groups",
+        color: "green"
+      },
+      "ORDINAL": {
+        tooltip: "Ordinal Value",
+        color: "orange"
+      },
       // ORG - see above.
-      "PERCENT": {tooltip: "Percentage", color: "orange"},
-      "PRODUCT": {tooltip: "Product", color: "purple"},
-      "QUANTITY": {tooltip: "Quantity", color: "orange"},
-      "TIME": {tooltip: "Time", color: "fuchsia"},
-      "WORK_OF_ART": {tooltip: "Work of Art/Media", color: "tan"},
+      "PERCENT": {
+        tooltip: "Percentage",
+        color: "orange"
+      },
+      "PRODUCT": {
+        tooltip: "Product",
+        color: "purple"
+      },
+      "QUANTITY": {
+        tooltip: "Quantity",
+        color: "orange"
+      },
+      "TIME": {
+        tooltip: "Time",
+        color: "fuchsia"
+      },
+      "WORK_OF_ART": {
+        tooltip: "Work of Art/Media",
+        color: "tan"
+      },
     }
 
     const entity = token.entity;
