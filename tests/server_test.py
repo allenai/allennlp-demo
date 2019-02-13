05e853aad05770b47c73fd47db924b95e1a8277d
Brendan Roof <brendanr@allenai.org>
2019-02-13 15:14:36 -0800

    cleanup

--- tests/server_test.py
+++ tests/server_test.py
@@ -12,7 +12,6 @@ from allennlp.common.util import JsonDict
 from allennlp.common.testing import AllenNlpTestCase
 from allennlp.models.archival import load_archive
 from allennlp.service.predictors import Predictor
-from werkzeug.wrappers import BaseResponse
 
 import app
 from app import make_app
