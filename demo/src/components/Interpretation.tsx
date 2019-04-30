42f894d31cc38a07bba02233b89637f699f0d60e
Jens Tuyls <you@example.com>
2019-04-30 13:26:47 -0700

    updated changes

--- demo/src/components/Interpretation.tsx
+++ demo/src/components/Interpretation.tsx
@@ -2,32 +2,23 @@ import React from 'react'
 import colormap from 'colormap'
 import styled from 'styled-components';
 
-interface Props {
+interface Props extends DefaultProps {
   tokensWithWeights: {token: string, weight: number}[];
-  colorScheme: "jet" | "hsv" | "hot" | "cool" | "spring" |
-               "summer" | "autumn" | "winter" | "bone" | 
-               "copper" | "greys" | "YIGnBu" | "greens" |
-               "YIOrRd" | "bluered" | "RdBu" | "picnic" |
-               "rainbow" | "portland" | "blackbody" | "earth" |
-               "electric" | "viridis" | "inferno" | "magma" |
-               "plasma" | "warm" | "rainbow-soft" | "bathymetry" |
-               "cdom" | "chlorophyll" | "density" | "freesurface-blue" |
-               "freesurface-red" | "oxygen" | "par" | "phase" | "salinity" |
-               "temperature" | "turbidity" | "velocity-blue" |
-               "velocity-green" | "cubehelix";
+}
+
+interface DefaultProps {
+  colormap: ColorMap
+}
+
+interface ColorMap {
+  colorScheme: ColorScheme;
+  format: ColorMapFormat; 
   nShades: number;
 }
 
-const ColorizedToken = styled.span`
-  background-color: ${props => props.color};
-  padding: 5px;
-  margin: 5px;
-  display: inline-block;
-  border-radius: 3px;
-`;
+type ColorMapFormat = "hex" | "rgbaString" | "rba" | "float";
 
-type DefaultProps = {
-  colorScheme: "jet" | "hsv" | "hot" | "cool" | "spring" |
+type ColorScheme = "jet" | "hsv" | "hot" | "cool" | "spring" |
   "summer" | "autumn" | "winter" | "bone" | 
   "copper" | "greys" | "YIGnBu" | "greens" |
   "YIOrRd" | "bluered" | "RdBu" | "picnic" |
@@ -38,10 +29,8 @@ type DefaultProps = {
   "freesurface-red" | "oxygen" | "par" | "phase" | "salinity" |
   "temperature" | "turbidity" | "velocity-blue" |
   "velocity-green" | "cubehelix";
-  nShades: number;
-}
 
-class ExplanationSaliencyMap extends React.Component<Props> {
+class TextSaliencyMap extends React.Component<Props> {
   constructor(props: Props) {
     super(props)
 
@@ -49,21 +38,24 @@ class ExplanationSaliencyMap extends React.Component<Props> {
   }
 
   static defaultProps: DefaultProps = {
-    colorScheme: 'RdBu',
-    nShades: 20
+    colormap: {
+      colorScheme: 'RdBu',
+      format: 'hex',
+      nShades: 20
+    }
   }
 
   colorize(tokensWithWeights: {token: string, weight: number}[]) {
     let colors = colormap({
-      colormap: this.props.colorScheme,
-      format: 'hex',
-      nshades: this.props.nShades
+      colormap: this.props.colormap.colorScheme,
+      format: this.props.colormap.format,
+      nshades: Math.min(Math.max(this.props.colormap.nShades, 1), 72)
     })
 
     let result_string: JSX.Element[] = [];
     tokensWithWeights.forEach((obj: {token: string, weight: number}, idx: number) => {
       result_string.push(
-        <ColorizedToken color={obj['weight'] ? colors[Math.round(obj['weight'] * (this.props.nShades - 1))] : 'transparent'}
+        <ColorizedToken backgroundColor={obj['weight'] ? colors[Math.round(obj['weight'] * (this.props.colormap.nShades - 1))] : 'transparent'}
                         key={idx}>{obj['token']}
         </ColorizedToken>
       )
@@ -84,5 +76,16 @@ class ExplanationSaliencyMap extends React.Component<Props> {
   }
 }
 
+interface ColorizedTokenProps {
+  backgroundColor: string;
+}
 
-export default ExplanationSaliencyMap; 
+const ColorizedToken = styled.span`
+  background-color: ${(props:ColorizedTokenProps) => props.backgroundColor};
+  padding: 5px;
+  margin: 5px;
+  display: inline-block;
+  border-radius: 3px;
+`;
+
+export default TextSaliencyMap; 
