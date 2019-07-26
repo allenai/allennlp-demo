8829b5e79074253afbde21a243446650b2ded439
sanjays <sanjays@dhcp-v006-182.mobile.uci.edu>
2019-07-25 20:10:18 -0700

    Adding lm's

--- demo/src/models.js
+++ demo/src/models.js
@@ -12,7 +12,7 @@ import WikiTables from './components/demos/WikiTables';
 import Nlvr from './components/demos/Nlvr';
 import Atis from './components/demos/Atis';
 import QuarelZero from './components/demos/QuarelZero';
-import Gpt2 from './components/demos/Gpt2';
+import LanguageModel from './components/demos/LanguageModel';
 import MaskedLm from './components/demos/MaskedLm';
 import annotateIcon from './icons/annotate-14px.svg';
 import otherIcon from './icons/other-14px.svg';
@@ -63,7 +63,7 @@ const modelGroups = [
         defaultOpen: true,
         models: [
             {model: "event2mind", name: "Event2Mind", component: Event2Mind},
-            {model: "gpt2", name: "Gpt2", component: Gpt2},
+            {model: "next-token-lm", name: "Language Model", component: LanguageModel},
             {model: "masked-lm", name: "Masked Language Modeling", component: MaskedLm},
             {model: "user-models", name: "Your model here!"}
         ]
