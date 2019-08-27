0ba6e8a625fb6ddb4f2cb1e6a1909926a3f4d627
Joel Grus <joelgrus@gmail.com>
2019-08-27 06:20:39 -0700

    factor

--- demo/src/App.js
+++ demo/src/App.js
@@ -34,16 +34,15 @@ const App = () => (
             <Route exact path="/" render={() => (
               <Redirect to={DEFAULT_PATH}/>
             )}/>
-            <Route path="/nochrome/:model/:slug?"
-                   render={(routeProps) => <Demo {...routeProps}/>}/>
-            <Route path="/:model/:slug?" component={DemoChrome}/>
+            <Route path="/task/:model/:slug?" component={SingleTaskDemo}/>
+            <Route path="/:model/:slug?" component={Demo}/>
           </Switch>
         </BlockOverflow>
     </Router>
   </ThemeProvider>
 )
 
-const DemoChrome = (props) => {
+const Demo = (props) => {
   const [requestData, setRequestData] = useState(null)
   const [responseData, setResponseData] = useState(null)
 
@@ -64,13 +63,13 @@ const DemoChrome = (props) => {
       <Header alwaysVisible={true} />
       <div className="pane-container">
         <Menu selectedModel={model} clearData={clearData}/>
-        <Demo {...props} requestData={requestData} responseData={responseData} updateData={updateData}/>
+        <SingleTaskDemo {...props} requestData={requestData} responseData={responseData} updateData={updateData}/>
       </div>
     </React.Fragment>
   )
 }
 
-class Demo extends React.Component {
+class SingleTaskDemo extends React.Component {
   constructor(props) {
     super(props);
 
@@ -121,43 +120,39 @@ class Demo extends React.Component {
     const { slug, selectedModel } = this.state;
     const { requestData, responseData, updateData } = this.props
 
-    const ModelComponent = () => {
-      if (slug && !responseData) {
-        // We're still waiting for permalink data, so just return the placeholder component.
-        return (<WaitingForPermalink/>)
-      } else if (modelComponents[selectedModel]) {
-          // This is a model we know the component for, so render it.
-          return React.createElement(modelComponents[selectedModel], {requestData, responseData, selectedModel, updateData})
-      } else if (selectedModel === "user-models") {
-        const modelRequest = "User Contributed Models"
-        const modelDescription = (
+    if (slug && !responseData) {
+      // We're still waiting for permalink data, so just return the placeholder component.
+      return (<WaitingForPermalink/>)
+    } else if (modelComponents[selectedModel]) {
+        // This is a model we know the component for, so render it.
+        return React.createElement(modelComponents[selectedModel], {requestData, responseData, selectedModel, updateData})
+    } else if (selectedModel === "user-models") {
+      const modelRequest = "User Contributed Models"
+      const modelDescription = (
+        <span>
           <span>
-            <span>
-              AllenNLP is looking to add contributed models implemented using AllenNLP as either library components or demos (with free hosting!).
-              If you have a published result or novel model demonstrating strong performance on a dataset and you are interested
-              in adding your model to a list of publically available implementations, as a service to this demo, or as a component in the library itself,
-              please consider opening an issue on our
-            </span>
-            <ExternalLink href="https://github.com/allenai/allennlp/issues" target="_blank" rel="noopener">{' '} public Github repository </ExternalLink>
-            <span>
-              or sending us an email at allennlp-contact@allenai.org to discuss what you have in mind.
-            </span>
+            AllenNLP is looking to add contributed models implemented using AllenNLP as either library components or demos (with free hosting!).
+            If you have a published result or novel model demonstrating strong performance on a dataset and you are interested
+            in adding your model to a list of publically available implementations, as a service to this demo, or as a component in the library itself,
+            please consider opening an issue on our
           </span>
-        );
+          <ExternalLink href="https://github.com/allenai/allennlp/issues" target="_blank" rel="noopener">{' '} public Github repository </ExternalLink>
+          <span>
+            or sending us an email at allennlp-contact@allenai.org to discuss what you have in mind.
+          </span>
+        </span>
+      );
 
-        return (
-          <div className="model model__content">
-            <div className='model__content'>
-              <PaneTop>
-                <ModelIntro title={modelRequest} description={modelDescription}/>
-              </PaneTop>
-            </div>
+      return (
+        <div className="model model__content">
+          <div className='model__content'>
+            <PaneTop>
+              <ModelIntro title={modelRequest} description={modelDescription}/>
+            </PaneTop>
           </div>
-        )
-      }
+        </div>
+      )
     }
-
-    return <ModelComponent/>
   }
 }
 
