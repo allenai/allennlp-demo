7fc126535b62698fd2a296b61fd73adf374009fe
Michael Schmitz <michael@schmitztech.com>
2019-08-29 16:01:29 -0700

    Fixup Reading Comprehension.

--- demo/src/components/demos/ReadingComprehension.js
+++ demo/src/components/demos/ReadingComprehension.js
@@ -12,7 +12,9 @@ import OutputField from '../OutputField'
 import { API_ROOT } from '../../api-config';
 import { truncateText } from '../DemoInput'
 import { UsageSection } from '../UsageSection';
-import { CodeSnippet } from '../CodeSnippet';
+import { UsageHeader } from '../UsageHeader'
+import { UsageCode } from '../UsageCode';
+import SyntaxHighlight from '../highlight/SyntaxHighlight';
 
 const title = "Reading Comprehension"
 
@@ -453,33 +455,41 @@ const usage = (
     <UsageSection>
       <h3>Prediction</h3>
       <h5>On the command line (bash):</h5>
-      <CodeSnippet language="bash">
-        {`echo '{"passage": "The Matrix is a 1999 science fiction action film written and directed by The Wachowskis, starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano.", "question": "Who stars in The Matrix?"}' | \\
+      <UsageCode>
+        <SyntaxHighlight language="bash">
+          {`echo '{"passage": "The Matrix is a 1999 science fiction action film written and directed by The Wachowskis, starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano.", "question": "Who stars in The Matrix?"}' | \\
 allennlp predict https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2018.11.30-charpad.tar.gz -`}
-      </CodeSnippet>
+        </SyntaxHighlight>
+      </UsageCode>
       <h5>As a library (Python):</h5>
-      <CodeSnippet language="python">
-        {`from allennlp.predictors.predictor import Predictor
+      <UsageCode>
+        <SyntaxHighlight language="python">
+          {`from allennlp.predictors.predictor import Predictor
 predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2018.11.30-charpad.tar.gz")
 predictor.predict(
   passage="The Matrix is a 1999 science fiction action film written and directed by The Wachowskis, starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano.",
   question="Who stars in The Matrix?"
 )`}
-      </CodeSnippet>
+        </SyntaxHighlight>
+      </UsageCode>
     </UsageSection>
     <UsageSection>
       <h3>Evaluation</h3>
-      <CodeSnippet language="python">
-        {`allennlp evaluate \\
+      <UsageCode>
+        <SyntaxHighlight language="python">
+          {`allennlp evaluate \\
   https://s3-us-west-2.amazonaws.com/allennlp/models/bidaf-model-2017.09.15-charpad.tar.gz \\
   https://s3-us-west-2.amazonaws.com/allennlp/datasets/squad/squad-dev-v1.1.json`}
-      </CodeSnippet>
+        </SyntaxHighlight>
+      </UsageCode>
     </UsageSection>
     <UsageSection>
       <h3>Training</h3>
-      <CodeSnippet language="python">
-        allennlp train training_config/bidaf.jsonnet -s output_path
-      </CodeSnippet>
+      <UsageCode>
+        <SyntaxHighlight language="python">
+          allennlp train training_config/bidaf.jsonnet -s output_path
+        </SyntaxHighlight>
+      </UsageCode>
     </UsageSection>
   </React.Fragment>
 )
