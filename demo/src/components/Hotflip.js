581512fe79be6d9f68049964e0fecf0e58655475
Eric Wallace <ewallac2@umd.edu>
2019-09-24 17:07:38 -0400

    change wording for BERT targeted attack

--- demo/src/components/Hotflip.js
+++ demo/src/components/Hotflip.js
@@ -142,7 +142,7 @@ export default class HotflipComponent extends React.Component {
     const target = targeted === undefined ?
       " "
     :
-      <p> Change prediction to (leave blank to allow any change): <FormInput type="text" onChange={ this.updateTargetWord }/> </p>
+      <p> Leave blank to allow any change (untargeted). For targeted attacks, enter a single token that you want to flip the mask to: <FormInput type="text" onChange={ this.updateTargetWord }/> </p>
 
     const buttonDisplay = (flippedString !== " " && targeted === undefined) ?
       " "
