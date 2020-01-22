6360826d55580f342510eb90549e09baf76e3c8e
Slater <megan.e.slater@gmail.com>
2020-01-22 09:54:13 -0800

    remove unused imports

--- demo/src/components/demos/MaskedLm.js
+++ demo/src/components/demos/MaskedLm.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 import _ from 'lodash';
-import { Footer, ExternalLink } from '@allenai/varnish/components';
 
 import OutputField from '../OutputField'
 import { Accordion } from 'react-accessible-accordion';
