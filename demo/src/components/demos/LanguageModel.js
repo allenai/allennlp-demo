3b18c076591e83e552087423d625e7cd4df41981
Megan Slater <meslater1030@users.noreply.github.com>
2020-02-10 09:28:29 -0800

    give labels to all the sets of accordion menus (#310)

--- demo/src/components/demos/LanguageModel.js
+++ demo/src/components/demos/LanguageModel.js
@@ -219,7 +219,7 @@ const Attacks = ({attackData, attackModel, requestData}) => {
     hotflipData["final"][0] = cleanTokensForDisplay(hotflipData["final"][0]);
   }
   return (
-    <OutputField>
+    <OutputField label="Model Attacks">
       <Accordion accordion={false}>
         <HotflipComponent hotflipData={hotflipData} hotflipFunction={attackModel(requestData, HOTFLIP_ATTACKER, NAME_OF_INPUT_TO_ATTACK, NAME_OF_GRAD_INPUT)} />
       </Accordion>
