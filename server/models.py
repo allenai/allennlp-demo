a1e124071eb949bec6c1fd65b352f6e60b3a5cfd
Nitish Gupta <gnnitish@gmail.com>
2020-02-25 12:19:47 -0500

    overrides for model loading can be now set as a property in models.json

--- server/models.py
+++ server/models.py
@@ -34,11 +34,9 @@ def load_demo_models(models_file: str,
     for task_name in task_names:
         model = blob[task_name]
         model_type = model.get("type", "allennlp")
-        overrides = ""
 
         if task_name == "nmn-drop":
             util.import_submodules("semqa")
-            overrides = "{\"model\": { \"debug\":true}}"
 
         # If ever we introduce additional model types,
         # we'll need to add corresponding logic here.
@@ -53,7 +51,7 @@ def load_demo_models(models_file: str,
                     archive_file=model["archive_file"],
                     predictor_name=model["predictor_name"],
                     max_request_length=model["max_request_length"],
-                    overrides=overrides
+                    overrides=model.get("overrides", "")
         )
 
     return demo_models
