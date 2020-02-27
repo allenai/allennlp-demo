33b136efdc9dc6292363f943e0702380db695d9e
Michael Schmitz <michael@schmitztech.com>
2020-02-27 08:59:01 -0800

    Remove show more / less.

--- demo/src/components/ModelIntro.js
+++ demo/src/components/ModelIntro.js
@@ -26,14 +26,7 @@ class ModelIntro extends React.Component {
       <div>
         <h2><span>{title}</span></h2>
         <p>
-          {descriptionEllipsed
-            ? (
-              this.state.showFullDescription
-                ? <span>{description} <span><Button onClick={e => this.toggleShowMore()} ghost variant="link">Show Less</Button></span></span>
-                : <span>{descriptionEllipsed} <span><Button onClick={e => this.toggleShowMore()} ghost variant="link">Show More</Button></span></span>
-            )
-            : <span>{description}</span>
-          }
+          <span>{description}</span>
         </p>
       </div>
     );
