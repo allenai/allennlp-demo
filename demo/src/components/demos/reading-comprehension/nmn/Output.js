6ebd56ad7ec1812d0481d46d8cfe5430c2467f07
Sam Skjonsberg <sams@allenai.org>
2020-04-06 09:20:13 -0700

    Remove tab that displays all module output at once.

--- demo/src/components/demos/reading-comprehension/nmn/Output.js
+++ demo/src/components/demos/reading-comprehension/nmn/Output.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Checkbox, Tabs, Popover, Icon } from 'antd';
+import { Tabs, Popover } from 'antd';
 
 import OutputField from '../../../OutputField';
 import NestedHighlight from '../../../highlight/NestedHighlight';
@@ -137,154 +137,6 @@ export const StepOutput = ({ inputs, step }) => {
   );
 }
 
-export const MultiStepOutput = ({ inputs, steps }) => {
-  const range = [0, 1];
-  const values = [ 1e-8, 1 ];
-  const nf = Intl.NumberFormat('en-US', { maximumSignificantDigits: 4 });
-  const [ activeSteps, setActiveSteps ] = React.useState(steps);
-  return (
-    <>
-      <OutputField label="Modules">
-        {steps.map(step => {
-          const moduleInfo = ModuleInfo.findInfoByName(step.moduleName);
-          return (
-            <React.Fragment key={step.moduleName}>
-              <ModuleCheckbox
-                  onChange={e => {
-                    if (e.target.checked) {
-                      setActiveSteps(activeSteps.concat([ step ]));
-                    } else {
-                      setActiveSteps(activeSteps.filter(s => s !== step));
-                    }
-                  }}
-                  checked={activeSteps.find(s => s === step) ? true : false}>
-                <strong>{step.moduleName}</strong>
-                {moduleInfo ? (
-                  <Popover title={moduleInfo.signature} content={moduleInfo.description}>
-                    <ModuleInfoIcon />
-                  </Popover>
-                ) : null}
-              </ModuleCheckbox>
-            </React.Fragment>
-          )
-        })}
-      </OutputField>
-      <WithLogScaleSlider
-          label="Minimum Attention"
-          range={range}
-          values={values}
-          defaultValue={0.01}>{min => (
-        inputs.map((input, inputIdx) => {
-          // Compute the clusters that should be highlighted, given the selected minimum
-          // attention value.
-          const clusters = {};
-          // We also prepare a reverse map of values for each out by token index, as to
-          // display them in a tooltip that shows when hovering over an individual token.
-          const valuesByTokenIndex = {};
-
-          // We merge spans in the passage and question, but not elsewhere.
-          const shouldMergeSpans = input.name === 'question' || input.name === 'passage';
-
-          for(const step of steps) {
-            if (!activeSteps.find(s => s === step)) {
-              continue;
-            }
-            const outputs = step.getOutputsForInput(input.name);
-            for(const output of outputs) {
-              const label = output.label ? `${step.moduleName}_${output.label}` : step.moduleName;
-              const spans = [];
-              const lenValues = output.values.length;
-              let start = null;
-              for(const rawIdx in output.values) {
-                // When we iterate through the indices in this fashion in JavaScript, each index
-                // is a string. Convert it to an integer so things work as expected.
-                const index = parseInt(rawIdx);
-                const value = output.values[index];
-
-                // Show values when hovering a token, regardless of whether it's above the current
-                // filter value.
-                if (!Array.isArray(valuesByTokenIndex[index])) {
-                  valuesByTokenIndex[index] = [];
-                }
-                valuesByTokenIndex[index].push({ label, value });
-
-
-                if (shouldMergeSpans) {
-                  if (value >= min) {
-                    // Starting a new cluster
-                    if (start === null) {
-                      start = index;
-                    }
-
-                    // If we're on the last item, and it's active, and a span is currently open
-                    // we need to close it.
-                    if (index === lenValues - 1 && start !== null) {
-                      spans.push([ start, index ]);
-                      start = null;
-                    }
-                  } else {
-                    // Ending the current cluster
-                    if (start !== null) {
-                      spans.push([ start, index - 1 ]);
-                      start = null;
-                    }
-                  }
-                } else {
-                  if (value >= min) {
-                    spans.push([ index, index ]);
-                  }
-                }
-              }
-              if (Array.isArray(clusters[label])) {
-                clusters[label] = clusters[label].concat(spans);
-              } else {
-                clusters[label] = spans;
-              }
-            }
-          }
-
-          // For outputs that aren't the question or passage, use a custom token separator
-          const tokenSeparator = (
-            input.name === 'question' || input.name === 'passage'
-              ? undefined
-              : <>,&nbsp;</>
-          );
-          return (
-            <OutputField key={`${inputIdx}/${input.name}`} label={input.name}>
-              <SpacingFix>
-                <NestedHighlight
-                    tokens={input.tokens.map((t, i) => {
-                      const valuesForIdx = valuesByTokenIndex[i];
-
-                      // If there's no values for this token then don't wrap it in a <Popover />.
-                      if (!Array.isArray(valuesForIdx) || valuesForIdx.length === 0) {
-                        return t;
-                      }
-
-                      // Show a tooltip with the values for each token on hover.
-                      const content = (
-                        <>
-                          {valuesForIdx.map(({ label, value }, i) => (
-                            <div key={`${i}/${label}`}>
-                              <strong>{label}:</strong>&nbsp;{nf.format(value)}
-                            </div>
-                          ))}
-                        </>
-                      );
-
-                      return <Popover key={`${i}/${t}`} title={t} content={content}>{t}</Popover>;
-                    })}
-                    clusters={clusters}
-                    tokenSeparator={tokenSeparator} />
-              </SpacingFix>
-            </OutputField>
-          );
-        })
-      )}</WithLogScaleSlider>
-    </>
-  );
-}
-
 /**
  * @param {object} props
  * @param {object} props.response The raw response from the inference API.
@@ -299,44 +151,23 @@ export const Output = ({ response }) => {
       </OutputField>
       <OutputField label="Execution Steps">
         <Tabs animated={false}>
-          <Tabs.TabPane tab="Module by Module" key="by-module">
-            <Tabs animated={false}>
-              {explanation.steps.map((step, index) =>
-                <Tabs.TabPane
-                    tab={`${index + 1}. ${step.moduleName}`}
-                    key={`${index}-${step.moduleName}`}>
-                  <StepOutput inputs={explanation.inputs} step={step} />
-                </Tabs.TabPane>
-              )}
-              <Tabs.TabPane tab={<code>DEBUG</code>} key="debug">
-                <RawResponse>
-                  <pre>{JSON.stringify(response, null, 4)}</pre>
-                </RawResponse>
-              </Tabs.TabPane>
-            </Tabs>
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="All Modules" key="all-modules">
-            <MultiStepOutput inputs={explanation.inputs} steps={explanation.steps} />
+          {explanation.steps.map((step, index) =>
+            <Tabs.TabPane
+                tab={`${index + 1}. ${step.moduleName}`}
+                key={`${index}-${step.moduleName}`}>
+              <StepOutput inputs={explanation.inputs} step={step} />
+            </Tabs.TabPane>
+          )}
+          <Tabs.TabPane tab={<code>DEBUG</code>} key="debug">
+            <RawResponse>
+              <pre>{JSON.stringify(response, null, 4)}</pre>
+            </RawResponse>
           </Tabs.TabPane>
         </Tabs>
       </OutputField>
     </React.Fragment>
   );
 }
-
-const ModuleCheckbox = styled(Checkbox)`
-  ${({ theme }) => `
-    &:not(:last-child) {
-      margin-right: ${theme.spacing.md};
-    }
-  `}
-`;
-
-const ModuleInfoIcon = styled(Icon).attrs(() => ({ type: 'question-circle'}))`
-    ${({ theme }) => `
-      margin-left: ${theme.spacing.xs};
-    `}
-`;
 
 const RawResponse = styled.code`
   ${({ theme }) => `
