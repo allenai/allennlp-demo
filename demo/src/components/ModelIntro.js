b35462daacb1c7661958552774d9ccf622938fe6
Michael Schmitz <MichaelS@allenai.org>
2020-04-21 14:35:46 -0700

    Re-order demos. (#354)

--- demo/src/components/ModelIntro.js
+++ demo/src/components/ModelIntro.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from '@allenai/varnish/components';
 
 /*******************************************************************************
   <ModelIntro /> Component
