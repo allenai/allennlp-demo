cc7454f9e0907e4271ad94dac1899b0b83455a62
Sam Skjonsberg <sams@allenai.org>
2020-05-01 14:09:20 -0700

    Port the bidaf-elmo model to the new serving mechanism.
    This adds code for serving the bidaf-elmo model with the new
    mechanism.
    
    The hotflip attack test is currently disabled since it's not
    working.
    
    I also removed the `README`s for each individual endpoint as they're
    duplicative of the `README` in the `allennlp_demo` package. Having
    less means we're more likely to keep them up to date.

---
+++ allennlp_demo/bidaf_elmo/test_api.py
@@ -0,0 +1,15 @@
+import pytest
+
+from allennlp_demo.bidaf_elmo.api import BidafElmoModelEndpoint
+from allennlp_demo.common.testing import ModelEndpointTests
+from typing import List
+
+@pytest.fixture
+def client():
+    endpoint = BidafElmoModelEndpoint()
+    return endpoint.app.test_client()
+
+class TestBidafElmoModelEndpoint(ModelEndpointTests):
+    def attacker_ids() -> List[str]:
+        # The hotflip attack for this model is currently broken.
+        return [ aid for aid in super().attacker_ids() if aid != "hotflip" ]
