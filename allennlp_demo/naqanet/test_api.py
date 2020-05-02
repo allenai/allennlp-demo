8f22145a403197e3a1498abb736bb8cd58f4533b
Sam Skjonsberg <sams@allenai.org>
2020-05-01 17:09:40 -0700

    Port NAQANet to the new serving mechanism. (#393)
    This adds code for serving the NAQANet reading comprehension model
    via the new mechanism.
    
    The hotflip attacker is again disabled due to an outstanding bug,
    and prediction test needed to be modified as the output is different.

---
+++ allennlp_demo/naqanet/test_api.py
@@ -0,0 +1,25 @@
+import pytest
+
+from flask.testing import FlaskClient
+from allennlp_demo.common.testing import ModelEndpointTests
+from allennlp_demo.naqanet.api import NAQANetModelEndpoint
+from typing import List
+
+@pytest.fixture
+def client():
+    endpoint = NAQANetModelEndpoint()
+    return endpoint.app.test_client()
+
+class TestNAQANetModelEndpoint(ModelEndpointTests):
+    def attacker_ids(self) -> List[str]:
+        # The hotflip attack for this model is currently broken.
+        return [ aid for aid in super().attacker_ids() if aid != "hotflip" ]
+
+    def test_predict(self, client: FlaskClient):
+        resp = client.post("/predict", query_string={ "no_cache": True },
+                           json=self.rc_input())
+        assert resp.status_code == 200
+        assert resp.json is not None
+        assert "answer" in resp.json
+        assert len(resp.json["passage_question_attention"]) > 0
+
