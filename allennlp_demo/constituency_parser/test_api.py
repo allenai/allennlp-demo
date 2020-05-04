7c8acfa341c301e87f870511acb79ffdb06241cb
Michael Schmitz <michaels@allenai.org>
2020-05-04 23:13:29 +0000

    Fixups

--- allennlp_demo/constituency_parser/test_api.py
+++ allennlp_demo/constituency_parser/test_api.py
@@ -1,7 +1,7 @@
 import pytest
 
 from allennlp_demo.common.testing import ModelEndpointTests
-from allennlp_demo.constiency_parser.api import ConstituencyParserModelEndpoint
+from allennlp_demo.constituency_parser.api import ConstituencyParserModelEndpoint
 
 
 @pytest.fixture
