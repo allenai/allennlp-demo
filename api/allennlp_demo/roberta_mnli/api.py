0426ab68ecbfdc45030637cdfcf31e8cd30b985c
epwalsh <epwalsh10@gmail.com>
2020-05-11 14:32:41 -0700

    add roberta mnli

---
+++ api/allennlp_demo/roberta_mnli/api.py
@@ -0,0 +1,16 @@
+import os
+
+from allennlp_models import nli  # noqa: F401
+
+from allennlp_demo.common import config, http
+
+
+class RobertaMnliModelEndpoint(http.ModelEndpoint):
+    def __init__(self):
+        c = config.Model.from_file(os.path.join(os.path.dirname(__file__), "model.json"))
+        super().__init__(c)
+
+
+if __name__ == "__main__":
+    endpoint = RobertaMnliModelEndpoint()
+    endpoint.run()
