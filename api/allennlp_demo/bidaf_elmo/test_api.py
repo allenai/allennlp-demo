8399e0b40f6ee55933a90e9391c02c333ec47ee4
epwalsh <epwalsh10@gmail.com>
2020-05-12 13:58:28 -0700

    skip slow elmo tests

--- api/allennlp_demo/bidaf_elmo/test_api.py
+++ api/allennlp_demo/bidaf_elmo/test_api.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from overrides import overrides
+import pytest
 
 from allennlp_demo.bidaf_elmo.api import BidafElmoModelEndpoint
 from allennlp_demo.common.testing import RcModelEndpointTestCase
@@ -13,3 +14,13 @@ class TestBidafElmoModelEndpoint(RcModelEndpointTestCase):
     def attacker_ids(self) -> List[str]:
         # The hotflip attack for this model is currently broken.
         return [aid for aid in super().attacker_ids() if aid != "hotflip"]
+
+    @pytest.mark.skip("Takes too long")
+    @overrides
+    def test_interpret(self):
+        pass
+
+    @pytest.mark.skip("Takes too long")
+    @overrides
+    def test_attack(self):
+        pass
