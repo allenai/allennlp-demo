ea4d3923a496172f3c0aee976c98749a3a8e284a
Sam Skjonsberg <sams@allenai.org>
2020-05-12 17:27:15 -0700

    Override the `load_attackers()` method for the LM endpoints.
    The LM endpoints don't have input reduction attackers and require
    a custom `vocab_namespace` argument at instantiation of the `Hotflip`
    attacker.  This change provides that by overriding `load_attackers()`
    for the LM endpoints.

--- api/allennlp_demo/masked_lm/api.py
+++ api/allennlp_demo/masked_lm/api.py
@@ -1,4 +1,7 @@
 import os
+from typing import Mapping
+
+from allennlp.interpret.attackers import Attacker, Hotflip
 
 from allennlp_demo.common import config, http
 
@@ -7,6 +10,11 @@ class MaskedLmModelEndpoint(http.ModelEndpoint):
     def __init__(self):
         c = config.Model.from_file(os.path.join(os.path.dirname(__file__), "model.json"))
         super().__init__(c)
+
+    def load_attackers(self) -> Mapping[str, Attacker]:
+        hotflip = Hotflip(self.predictor, "bert")
+        hotflip.initialize()
+        return {"hotflip": hotflip}
 
 
 if __name__ == "__main__":
--- api/allennlp_demo/next_token_lm/api.py
+++ api/allennlp_demo/next_token_lm/api.py
@@ -1,6 +1,8 @@
 import os
+from typing import Mapping
 
 from allennlp_models import lm  # noqa: F401
+from allennlp.interpret.attackers import Attacker, Hotflip
 
 from allennlp_demo.common import config, http
 
@@ -9,6 +11,11 @@ class NextTokenLmModelEndpoint(http.ModelEndpoint):
     def __init__(self):
         c = config.Model.from_file(os.path.join(os.path.dirname(__file__), "model.json"))
         super().__init__(c)
+
+    def load_attackers(self) -> Mapping[str, Attacker]:
+        hotflip = Hotflip(self.predictor, "gpt2")
+        hotflip.initialize()
+        return {"hotflip": hotflip}
 
 
 if __name__ == "__main__":
