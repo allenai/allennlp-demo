3b9a61d37382309de867b0c467060b6be7ca7b5f
epwalsh <epwalsh10@gmail.com>
2020-05-14 11:02:27 -0700

    make endpoint info requests concurrently

--- api/allennlp_demo/info/prod_api.py
+++ api/allennlp_demo/info/prod_api.py
@@ -6,19 +6,11 @@ The workload for this service is entirely I/O bound, so should improve the
 service's ability to handle more concurrent connections without using a
 multi-process WSGI server like `gunicorn`.
 """
-from gevent import monkey, pywsgi
+from gevent import pywsgi
+import kubernetes
 
-# This ensures third party libs use the non-blocking socket, so that we actually
-# benefit from using `gevent`. They recommend that it be one of the first
-# lines executed in your program, which is why we import it and immediately
-# execute the command.
-#
-# See: http://www.gevent.org/intro.html#monkey-patching
-monkey.patch_all()
+from allennlp_demo.info.api import InfoService
 
-import kubernetes  # noqa: E402
-
-from allennlp_demo.info.api import InfoService  # noqa: E402
 
 if __name__ == "__main__":
     kubernetes.config.load_incluster_config()
