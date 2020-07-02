5b52e12936c8db10eb6d001824870f52b9f144d1
Dirk Groeneveld <dirkg@allenai.org>
2020-07-02 01:32:29 -0700

    Productivity through formatting

--- api/allennlp_demo/fine_grained_ner/api.py
+++ api/allennlp_demo/fine_grained_ner/api.py
@@ -3,6 +3,7 @@ import os
 from allennlp_demo.common import config, http
 from allennlp_models import tagging  # noqa: F401
 
+
 class FineGrainedNerModelEndpoint(http.ModelEndpoint):
     def __init__(self):
         # TODO: should just import the exact submodule we need.
