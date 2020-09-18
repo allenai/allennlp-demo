8187431f9889fb75331a5c120799ffd5ec1b15cc
Dirk Groeneveld <dirkg@allenai.org>
2020-09-17 18:14:32 -0700

    Read base64 images

--- api/allennlp_demo/vilbert_vqa/api.py
+++ api/allennlp_demo/vilbert_vqa/api.py
@@ -1,5 +1,8 @@
 import os
 
+import tempfile
+from base64 import standard_b64decode
+
 from allennlp.common.util import JsonDict
 from allennlp_models import rc  # noqa: F401
 
@@ -12,7 +15,24 @@ class VilbertVqaModelEndpoint(http.ModelEndpoint):
         super().__init__(c)
 
     def predict(self, inputs: JsonDict):
-        return super().predict(inputs)
+        image_url = inputs.get("image_url")
+        if image_url is not None:
+            return super().predict({
+                "question": inputs["question"],
+                "image": image_url
+            })
+
+        image_base64 = inputs.get("image_base64")
+        if image_base64 is not None:
+            with tempfile.NamedTemporaryFile(prefix=f"{self.__class__.__name__}-") as f:
+                f.write(standard_b64decode(image_base64))
+                f.flush()
+                return super().predict({
+                    "question": inputs["question"],
+                    "image": f.name
+                })
+
+        raise ValueError("No image found in request.")
 
     def load_interpreters(self):
         # The interpreters don't work with this model right now.
