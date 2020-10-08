4bedad004276d3bb2df7cc7ce5004742dc04969b
epwalsh <epwalsh10@gmail.com>
2020-10-07 21:20:05 -0700

    fix Vilbert VQA test

--- api/allennlp_demo/vilbert_vqa/test_api.py
+++ api/allennlp_demo/vilbert_vqa/test_api.py
@@ -1,3 +1,5 @@
+from overrides import overrides
+
 from allennlp_demo.common.testing import RcModelEndpointTestCase
 from allennlp_demo.vilbert_vqa.api import VilbertVqaModelEndpoint
 
@@ -5,8 +7,23 @@ from allennlp_demo.vilbert_vqa.api import VilbertVqaModelEndpoint
 class TestVilbertVqaModelEndpoint(RcModelEndpointTestCase):
     endpoint = VilbertVqaModelEndpoint()
 
+    predict_input = {
+        "question": "What game are they playing?",
+        "image_url": "https://storage.googleapis.com/allennlp-public-data/vqav2/baseball.jpg",
+    }
+
+    @overrides
+    def check_predict_result(self, result):
+        assert len(result) > 0
+        for answer in result:
+            assert "answer" in answer
+            assert "confidence" in answer
+            assert 0.0 <= answer["confidence"] <= 100.0
+
+    @overrides
     def interpreter_ids(self):
         return []
 
+    @overrides
     def attacker_ids(self):
         return []
