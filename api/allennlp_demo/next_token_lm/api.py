3ae2b1acdc0e70da500d456abee8a61c0e65e5e1
epwalsh <epwalsh10@gmail.com>
2020-10-15 15:20:27 -0700

    fix trailing space issue

--- api/allennlp_demo/next_token_lm/api.py
+++ api/allennlp_demo/next_token_lm/api.py
@@ -2,6 +2,8 @@ import os
 from typing import Dict
 
 from allennlp.interpret.attackers import Attacker, Hotflip
+from allennlp.predictors.predictor import JsonDict
+from overrides import overrides
 
 from allennlp_demo.common import config, http
 
@@ -11,10 +13,19 @@ class NextTokenLmModelEndpoint(http.ModelEndpoint):
         c = config.Model.from_file(os.path.join(os.path.dirname(__file__), "model.json"))
         super().__init__(c)
 
+    @overrides
     def load_attackers(self) -> Dict[str, Attacker]:
         hotflip = Hotflip(self.predictor, "gpt2")
         hotflip.initialize()
         return {"hotflip": hotflip}
+
+    @overrides
+    def predict(self, inputs: JsonDict) -> JsonDict:
+        # We override this to do a little extra sanitization on the inputs.
+        # In particular, we strip any trailing whitespace (except for newlines,
+        # which are most likely intentional) since this results in poor predictions.
+        inputs["sentence"] = inputs["sentence"].rstrip(" \t\r")
+        return self.predictor.predict_json(inputs)
 
 
 if __name__ == "__main__":
