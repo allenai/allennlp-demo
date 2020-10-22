4a7586cb91bcc784a83a42ce9b00a5862ab95f39
Evan Pete Walsh <epwalsh10@gmail.com>
2020-10-22 11:06:09 -0700

    Update GPT-2 demo with beam search (#565)
    * update AllenNLP(models)
    
    * update AllenNLP, use allennlp_models.pretrained
    
    * fix
    
    * update semparse demos
    
    * add model endpoint tests in GitHub Actions
    
    * fix up
    
    * try force test single endpoint
    
    * try again
    
    * fixes
    
    * different method for testing
    
    * try matrix
    
    * clean up
    
    * add some tests
    
    * updates
    
    * fix validation step
    
    * update models req
    
    * fix nmn_drop
    
    * fixes
    
    * no hotflip for roberta snli/mnli
    
    * fix
    
    * use old load method for vilbert
    
    * fix Vilbert VQA test
    
    * update docs
    
    * update allennlp-semparse
    
    * run on schedule
    
    * Sam's comments and some more fixes
    
    * update GPT-2 demo with beam search
    
    * fix trailing space issue
    
    * improve sanitization
    
    * sanitize outputs
    
    * updated UI with sequences and predictions (#570)
    
    * update requirements
    
    Co-authored-by: jvstokes <40584422+jvstokes@users.noreply.github.com>

--- api/allennlp_demo/next_token_lm/api.py
+++ api/allennlp_demo/next_token_lm/api.py
@@ -1,20 +1,63 @@
 import os
-from typing import Dict
+import re
+from typing import Dict, Any
 
 from allennlp.interpret.attackers import Attacker, Hotflip
+from allennlp.predictors.predictor import JsonDict
+from overrides import overrides
 
 from allennlp_demo.common import config, http
 
 
 class NextTokenLmModelEndpoint(http.ModelEndpoint):
+    # TODO: don't hardcode this.
+    _END_OF_TEXT_TOKEN = "<|endoftext|>"
+
     def __init__(self):
         c = config.Model.from_file(os.path.join(os.path.dirname(__file__), "model.json"))
         super().__init__(c)
 
+    @overrides
     def load_attackers(self) -> Dict[str, Attacker]:
         hotflip = Hotflip(self.predictor, "gpt2")
         hotflip.initialize()
         return {"hotflip": hotflip}
+
+    @overrides
+    def predict(self, inputs: JsonDict) -> JsonDict:
+        # We override this to do a little extra sanitization on the inputs.
+        # In particular, we strip any trailing whitespace (except for newlines,
+        # which are most likely intentional) and remove any double spaces, since
+        # these things result in poor predictions.
+        inputs["sentence"] = self._sanitize_input_text(inputs["sentence"])
+        outputs = self.predictor.predict_json(inputs)
+        # We also do some final sanitization on the outputs to remove the '<endoftext>' token
+        # and filter out any predicted sequences that are empty, i.e. just equal to the
+        # '<|endoftext|>' token repeated.
+        return self._sanitize_outputs(outputs)
+
+    @staticmethod
+    def _sanitize_input_text(sentence: str) -> str:
+        return re.sub(r" +", " ", sentence.rstrip(" \t\r"))
+
+    def _sanitize_outputs(self, output: Dict[str, Any]) -> Dict[str, Any]:
+        sanitized_top_tokens = []
+        sanitized_top_indices = []
+        sanitized_probabilities = []
+        for tokens, indices, probability in zip(
+            output["top_tokens"], output["top_indices"], output["probabilities"]
+        ):
+            filtered = [(t, i) for (t, i) in zip(tokens, indices) if t != self._END_OF_TEXT_TOKEN]
+            if not filtered:
+                continue
+            new_tokens, new_indices = zip(*filtered)
+            sanitized_top_tokens.append(list(new_tokens))
+            sanitized_top_indices.append(list(new_indices))
+            sanitized_probabilities.append(probability)
+        output["top_tokens"] = sanitized_top_tokens
+        output["top_indices"] = sanitized_top_indices
+        output["probabilities"] = sanitized_probabilities
+        return output
 
 
 if __name__ == "__main__":
--- api/allennlp_demo/next_token_lm/test_api.py
+++ api/allennlp_demo/next_token_lm/test_api.py
@@ -1,3 +1,5 @@
+import pytest
+
 from allennlp_demo.common.testing import ModelEndpointTestCase
 from allennlp_demo.next_token_lm.api import NextTokenLmModelEndpoint
 
@@ -5,3 +7,76 @@ from allennlp_demo.next_token_lm.api import NextTokenLmModelEndpoint
 class TestNextTokenLmModelEndpoint(ModelEndpointTestCase):
     endpoint = NextTokenLmModelEndpoint()
     predict_input = {"sentence": "AlleNLP is a"}
+
+    @pytest.mark.parametrize(
+        "input_text, result",
+        [
+            (
+                "AllenNLP is one  of  the  most  popular ",
+                "AllenNLP is one of the most popular",
+            ),
+            ("AllenNLP is a framework.\n", "AllenNLP is a framework.\n"),
+            ("AllenNLP is a framework.\t", "AllenNLP is a framework."),
+        ],
+    )
+    def test_sanitize_input_text(self, input_text: str, result: str):
+        assert self.endpoint._sanitize_input_text(input_text) == result
+
+    @pytest.mark.parametrize(
+        "output, result",
+        [
+            (
+                {
+                    "top_indices": [
+                        [50256, 50256, 50256, 50256, 50256],
+                        [679, 318, 262, 1772, 286],
+                        [679, 318, 257, 2888, 286],
+                        [679, 318, 635, 257, 2888],
+                        [679, 318, 635, 262, 1772],
+                    ],
+                    "probabilities": [
+                        0.1311192363500595,
+                        0.00410857517272234,
+                        0.002775674918666482,
+                        0.0008492876659147441,
+                        0.00035627084434963763,
+                    ],
+                    "top_tokens": [
+                        [
+                            "<|endoftext|>",
+                            "<|endoftext|>",
+                            "<|endoftext|>",
+                            "<|endoftext|>",
+                            "<|endoftext|>",
+                        ],
+                        ["ĠHe", "Ġis", "Ġthe", "Ġauthor", "Ġof"],
+                        ["ĠHe", "Ġis", "Ġa", "Ġmember", "Ġof"],
+                        ["ĠHe", "Ġis", "Ġalso", "Ġa", "Ġmember"],
+                        ["ĠHe", "Ġis", "Ġalso", "Ġthe", "Ġauthor"],
+                    ],
+                },
+                {
+                    "top_indices": [
+                        [679, 318, 262, 1772, 286],
+                        [679, 318, 257, 2888, 286],
+                        [679, 318, 635, 257, 2888],
+                        [679, 318, 635, 262, 1772],
+                    ],
+                    "probabilities": [
+                        0.00410857517272234,
+                        0.002775674918666482,
+                        0.0008492876659147441,
+                        0.00035627084434963763,
+                    ],
+                    "top_tokens": [
+                        ["ĠHe", "Ġis", "Ġthe", "Ġauthor", "Ġof"],
+                        ["ĠHe", "Ġis", "Ġa", "Ġmember", "Ġof"],
+                        ["ĠHe", "Ġis", "Ġalso", "Ġa", "Ġmember"],
+                        ["ĠHe", "Ġis", "Ġalso", "Ġthe", "Ġauthor"],
+                    ],
+                },
+            )
+        ],
+    )
+    def test_sanitize_outputs(self, output, result):
+        assert self.endpoint._sanitize_outputs(output) == result
