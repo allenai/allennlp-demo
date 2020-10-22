5f8f9e14a984ed240500940ab7deb8423b19645a
epwalsh <epwalsh10@gmail.com>
2020-10-21 17:22:03 -0700

    improve sanitization

--- api/allennlp_demo/next_token_lm/api.py
+++ api/allennlp_demo/next_token_lm/api.py
@@ -1,4 +1,5 @@
 import os
+import re
 from typing import Dict
 
 from allennlp.interpret.attackers import Attacker, Hotflip
@@ -10,7 +11,9 @@ from allennlp_demo.common import config, http
 
 class NextTokenLmModelEndpoint(http.ModelEndpoint):
     def __init__(self):
-        c = config.Model.from_file(os.path.join(os.path.dirname(__file__), "model.json"))
+        c = config.Model.from_file(
+            os.path.join(os.path.dirname(__file__), "model.json")
+        )
         super().__init__(c)
 
     @overrides
@@ -23,9 +26,14 @@ class NextTokenLmModelEndpoint(http.ModelEndpoint):
     def predict(self, inputs: JsonDict) -> JsonDict:
         # We override this to do a little extra sanitization on the inputs.
         # In particular, we strip any trailing whitespace (except for newlines,
-        # which are most likely intentional) since this results in poor predictions.
-        inputs["sentence"] = inputs["sentence"].rstrip(" \t\r")
+        # which are most likely intentional) and remove any double spaces, since
+        # these things result in poor predictions.
+        inputs["sentence"] = self._sanitize_input_text(inputs["sentence"])
         return self.predictor.predict_json(inputs)
+
+    @staticmethod
+    def _sanitize_input_text(sentence: str) -> str:
+        return re.sub(r" +", " ", sentence.rstrip(" \t\r"))
 
 
 if __name__ == "__main__":
--- api/allennlp_demo/next_token_lm/test_api.py
+++ api/allennlp_demo/next_token_lm/test_api.py
@@ -1,3 +1,5 @@
+import pytest
+
 from allennlp_demo.common.testing import ModelEndpointTestCase
 from allennlp_demo.next_token_lm.api import NextTokenLmModelEndpoint
 
@@ -5,3 +7,17 @@ from allennlp_demo.next_token_lm.api import NextTokenLmModelEndpoint
 class TestNextTokenLmModelEndpoint(ModelEndpointTestCase):
     endpoint = NextTokenLmModelEndpoint()
     predict_input = {"sentence": "AlleNLP is a"}
+
+    @pytest.mark.parametrize(
+        "input_text, result",
+        [
+            (
+                "AllenNLP is one  of  the  most  popular ",
+                "AllenNLP is one of the most popular",
+            ),
+            ("AllenNLP is a framework.\n", "AllenNLP is a framework.\n"),
+            ("AllenNLP is a framework.\t", "AllenNLP is a framework."),
+        ],
+    )
+    def test_sanitize_input_text(self, input_text: str, result: str):
+        assert self.endpoint._sanitize_input_text(input_text) == result
