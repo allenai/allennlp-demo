bd693db428c8f0fe4309cecc8368933b00c1aa0b
epwalsh <epwalsh10@gmail.com>
2020-10-21 18:17:29 -0700

    sanitize outputs

--- api/allennlp_demo/next_token_lm/api.py
+++ api/allennlp_demo/next_token_lm/api.py
@@ -1,6 +1,6 @@
 import os
 import re
-from typing import Dict
+from typing import Dict, Any
 
 from allennlp.interpret.attackers import Attacker, Hotflip
 from allennlp.predictors.predictor import JsonDict
@@ -10,10 +10,11 @@ from allennlp_demo.common import config, http
 
 
 class NextTokenLmModelEndpoint(http.ModelEndpoint):
+    # TODO: don't hardcode this.
+    _END_OF_TEXT_TOKEN = "<|endoftext|>"
+
     def __init__(self):
-        c = config.Model.from_file(
-            os.path.join(os.path.dirname(__file__), "model.json")
-        )
+        c = config.Model.from_file(os.path.join(os.path.dirname(__file__), "model.json"))
         super().__init__(c)
 
     @overrides
@@ -29,11 +30,34 @@ class NextTokenLmModelEndpoint(http.ModelEndpoint):
         # which are most likely intentional) and remove any double spaces, since
         # these things result in poor predictions.
         inputs["sentence"] = self._sanitize_input_text(inputs["sentence"])
-        return self.predictor.predict_json(inputs)
+        outputs = self.predictor.predict_json(inputs)
+        # We also do some final sanitization on the outputs to remove the '<endoftext>' token
+        # and filter out any predicted sequences that are empty, i.e. just equal to the
+        # '<|endoftext|>' token repeated.
+        return self._sanitize_outputs(outputs)
 
     @staticmethod
     def _sanitize_input_text(sentence: str) -> str:
         return re.sub(r" +", " ", sentence.rstrip(" \t\r"))
+
+    def _sanitize_outputs(self, output: Dict[str, Any]) -> Dict[str, Any]:
+        sanitized_top_tokens = []
+        sanitized_top_indices = []
+        sanitized_probabilities = []
+        for tokens, indices, probability in zip(
+            output["top_tokens"], output["top_indices"], output["probabilities"]
+        ):
+            filtered = [(t, i) for (t, i) in zip(tokens, indices) if t != self._END_OF_TEXT_TOKEN]
+            if not filtered:
+                continue
+            new_tokens, new_indices = zip(*filtered)
+            sanitized_top_tokens.append(list(new_tokens))
+            sanitized_top_indices.append(list(new_indices))
+            sanitized_probabilities.append(probability)
+        output["top_tokens"] = sanitized_top_tokens
+        output["top_indices"] = sanitized_top_indices
+        output["probabilities"] = sanitized_probabilities
+        return output
 
 
 if __name__ == "__main__":
