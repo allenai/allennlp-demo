6df10bf69131adf6a7e5f2b0074d3829f65cca26
Sam Skjonsberg <sams@allenai.org>
2020-11-11 16:22:41 -0800

    Revert "Add the Varnish App."
    This reverts commit 7430fc2d77876800d0130760c4637464c26b138c.

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { VarnishApp } from '@allenai/varnish/components';
 import App from './App';
 
-ReactDOM.render(<VarnishApp><App /></VarnishApp>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
