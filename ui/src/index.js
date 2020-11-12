7430fc2d77876800d0130760c4637464c26b138c
Sam Skjonsberg <sams@allenai.org>
2020-11-11 16:16:23 -0800

    Add the Varnish App.

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { VarnishApp } from '@allenai/varnish/components';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<VarnishApp><App /></VarnishApp>, document.getElementById('root'));
