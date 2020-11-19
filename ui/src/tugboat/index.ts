ae93b56acdf887ffb1f9661f755b189d275ed0cd
Sam Skjonsberg <sams@allenai.org>
2020-11-19 13:36:57 -0800

    Drop custom form code.

--- ui/src/tugboat/index.ts
+++ ui/src/tugboat/index.ts
@@ -1,19 +1,3 @@
-import { Form as FormImpl, Field, Label, TextArea, Input, Select, OptDesc } from './form';
-
-// Adding statics so that we can export a module that is useable as:
-// <Form>
-//   <Form.Field ...>
-// </Form>
-const Form = FormImpl as any; // any is so we can add the random statics below
-Form.Field = Field;
-Form.Label = Label;
-Form.TextArea = TextArea;
-Form.Input = Input;
-Form.Select = Select;
-Form.OptDesc = OptDesc;
-
-export { Form };
-
 export * from './shared';
 export * from './Markdown';
 export * from './ModelUsageModal';
