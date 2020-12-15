dcc25023136e9a4a53a69408dbb66f0da9250073
Sam Skjonsberg <sams@allenai.org>
2020-12-15 10:52:37 -0800

    WIP: Add a new endpoint for listing tasks; Update to AllenNLP 1.2.2.
    This adds a new endpoint for listing the result of `get_tasks()` so
    that the information can be used in the UI.
    
    It doesn't work quite yet, because of bug where the `taskcards`
    aren't packaged up in the PyPi package for `allennlp_models`.
    The AllenNLP team is aware and will release a new version that
    fixes things, at which point we'll update to that version.

---
+++ api/allennlp_demo/tasks/api.py
@@ -0,0 +1,30 @@
+"""
+The tasks endpoint lists all demo tasks and some info about them.
+"""
+import logging  # noqa: E402
+import flask  # noqa: E402
+
+from allennlp_demo.common.logs import configure_logging  # noqa: E402
+from allennlp_models.pretrained import get_tasks # noqa: E402
+from allennlp_models.version import VERSION
+
+logger = logging.getLogger(__name__)
+
+class TasksService(flask.Flask):
+    def __init__(self, name: str = "info"):
+        super().__init__(name)
+        configure_logging(self)
+
+        @self.route("/", methods=["GET"])
+        def info():
+            return flask.jsonify({ "id": "tasks", "allennlp_models": VERSION })
+
+        @self.route("/tasks", methods=["GET"])
+        def tasks():
+            tasks = get_tasks()
+            print(tasks)
+            return flask.jsonify(tasks)
+
+if __name__ == "__main__":
+    app = TasksService()
+    app.run(host="0.0.0.0", port=8000)
