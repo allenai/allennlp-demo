a219a7a6727f526e4083f0bdd72abfefa4c41193
jon borchardt <jon.borchardt@gmail.com>
2020-12-23 15:28:09 -0800

    pr fix

--- ui/src/@types/colormap.d.ts
+++ ui/src/@types/colormap.d.ts
@@ -2,6 +2,6 @@
  * colormap does not have types: https://www.npmjs.com/package/colormap
  */
 declare module 'colormap' {
-    const src: any;
-    export default src;
+    const createColorMap: (spec: any) => any;
+    export default createColorMap;
 }
