6047042fed2a9c73e13b094b2faf4f353e10e0a4
Sam Skjonsberg <sams@allenai.org>
2021-01-04 09:47:37 -0800

    Declare an error locally where it's used. Some quick usage cleanup.

--- ui/src/demos/reading-comprehension/Usage.tsx
+++ ui/src/demos/reading-comprehension/Usage.tsx
@@ -7,48 +7,70 @@ import React from 'react';
 
 import { ModelUsage } from '../../components';
 import { Models, Examples } from '../../tugboat/context';
-import { NoSelectedModelError, InvalidExamplesFormatError } from '../../tugboat/error';
+import { NoSelectedModelError } from '../../tugboat/error';
 import { isGroupedExamples } from '../../tugboat/lib';
+
+class UngroupedExamplesError extends Error {
+    constructor() {
+        super(`The examples aren't grouped but are expected to be.`);
+    }
+}
 
 export const Usage = () => {
     const models = React.useContext(Models);
-    const examples = React.useContext(Examples);
+    const { examples } = React.useContext(Examples);
+
     if (!models.selectedModel) {
         throw new NoSelectedModelError();
     }
-    const x = examples.examples;
-    // Reading comp uses GroupedExamples
-    if (isGroupedExamples(x)) {
-        const ex = x['SQuAD-like Argument Finding'][2]; // matrix example
-        // TODO: we need to get these paths from model card
-        const evalDataPath =
-            'https://s3-us-west-2.amazonaws.com/allennlp/datasets/squad/squad-dev-v1.1.json';
-        const trainingDataPath =
-            'https://raw.githubusercontent.com/allenai/allennlp-models/v1.0.0/training_config/rc/bidaf_elmo.jsonnet';
-        return (
-            <ModelUsage
-                installCommand={'pip install allennlp==1.0.0 allennlp-models==1.0.0'}
-                bashCommand={`echo '{"passage": "${ex.passage.slice(0, 182)}.", "question": "${
-                    ex.question
-                }"}' | \\
-    allennlp predict ${models.selectedModel.card.archive_file} -`}
-                pythonCommand={`from allennlp.predictors.predictor import Predictor
-    import allennlp_models.rc
-    predictor = Predictor.from_path("${models.selectedModel.card.archive_file}")
-    predictor.predict(
-        passage="${ex.passage.slice(0, 182)}.",
-        question="${ex.question}"
-    )`}
-                evaluationCommand={`allennlp evaluate \\
-    ${models.selectedModel.card.archive_file} \\
-    ${evalDataPath}`}
-                trainingCommand={`allennlp train ${trainingDataPath} \\
-    -s output_path`}
-            />
-        );
-    } else {
-        throw new InvalidExamplesFormatError(
-            `The examples aren't grouped, but we expect them to be`
-        );
+
+    if (!isGroupedExamples(examples)) {
+        throw new UngroupedExamplesError();
     }
+
+    // TODO: This seems brittle. If the examples change this will fail at runtime.
+    const ex = examples['SQuAD-like Argument Finding'][2]; // matrix example
+
+    const installCommand = 'pip install allennlp==1.0.0 allennlp-models==1.0.0';
+
+    const bashCommand = `
+echo '{"passage": "${ex.passage.slice(0, 182)}.", "question": "${ex.question}"}' | \\
+    allennlp predict ${models.selectedModel.card.archive_file} -
+`.trim();
+
+    const pythonCommand = `
+from allennlp.predictors.predictor import Predictor
+import allennlp_models.rc
+
+predictor = Predictor.from_path("${models.selectedModel.card.archive_file}")
+predictor.predict(
+    passage="${ex.passage.slice(0, 182)}.",
+    question="${ex.question}"
+)`.trim();
+
+    // TODO: Get this from the model card.
+    const evalDataPath =
+        'https://s3-us-west-2.amazonaws.com/allennlp/datasets/squad/squad-dev-v1.1.json';
+    const evaluationCommand = `
+allennlp evaluate \\
+    ${models.selectedModel.card.archive_file} \\
+    ${evalDataPath}`.trim();
+
+    // TODO: Get this from the model card.
+    const trainingDataPath =
+        'https://raw.githubusercontent.com/allenai/allennlp-models/v1.0.0/training_config/rc/bidaf_elmo.jsonnet';
+    const trainingCommand = `allennlp train \\
+    ${trainingDataPath} \\
+    -s /path/to/output`.trim();
+
+    // TODO: The AllenNLP version could be pulled from the model's info route.
+    return (
+        <ModelUsage
+            installCommand={installCommand}
+            bashCommand={bashCommand}
+            pythonCommand={pythonCommand}
+            evaluationCommand={evaluationCommand}
+            trainingCommand={trainingCommand}
+        />
+    );
 };
