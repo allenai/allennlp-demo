b1089030ea0ffb89f134c41017a42f41912fa2e4
jon borchardt <jon.borchardt@gmail.com>
2021-01-07 13:30:50 -0800

    add model description
    added the description of the selected model
    
    changed the dropdown to use the short description if availiable
    
    pr clean up of typeguards

--- ui/src/tugboat/components/SelectModel.tsx
+++ ui/src/tugboat/components/SelectModel.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Form } from 'antd';
 
 import { Field, Select } from './form';
@@ -14,17 +15,38 @@ export const SelectModel = () => {
     return (
         <Form layout="vertical">
             <Field label="Model">
-                <Select
-                    value={ctx.selectedModel?.id}
-                    onChange={(id) => ctx.selectModelById(`${id}`)}>
-                    {ctx.models.map((m) => (
-                        <Select.Option key={m.id} value={m.id} label={m.card.display_name}>
-                            <b>{m.card.display_name}</b>
-                            <Markdown>{m.card.description}</Markdown>
-                        </Select.Option>
-                    ))}
-                </Select>
+                <Frame>
+                    <Select
+                        value={ctx.selectedModel?.id}
+                        onChange={(id) => ctx.selectModelById(`${id}`)}>
+                        {ctx.models.map((m) => (
+                            <Select.Option key={m.id} value={m.id} label={m.card.display_name}>
+                                <b>{m.card.display_name}</b>
+                                <Markdown>
+                                    {m.card.short_description || m.card.description}
+                                </Markdown>
+                            </Select.Option>
+                        ))}
+                    </Select>
+                    {ctx.selectedModel ? (
+                        <Description>
+                            <Markdown>{ctx.selectedModel.card.description}</Markdown>
+                        </Description>
+                    ) : null}
+                </Frame>
             </Field>
         </Form>
     );
 };
+
+const Frame = styled.div`
+    border: 1px solid ${({ theme }) => theme.color.N4};
+    background: ${({ theme }) => theme.color.N2};
+    border-radius: ${({ theme }) => theme.shape.borderRadius.default};
+`;
+
+const Description = styled.div`
+    padding: ${({ theme }) => theme.spacing.md};
+    max-height: 300px;
+    overflow-y: auto;
+`;
