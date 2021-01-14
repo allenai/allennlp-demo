d0908fd1f734e2815054ff55d58fbaa527d2f81b
Sam Skjonsberg <sams@allenai.org>
2021-01-13 19:30:09 -0800

    Source the model card from the new endpoint. (#715)
    This modifies the UI to use the new `/api/model-card` endpoint
    for retrieving model cards.
    
    There's a hack in place that maps `nmn` to an id of the
    appropriate form. Soon we'll be migrating to ids that all
    follow the `{task}-{model}` convention, at which point this
    hack can be removed.

---
+++ ui/src/lib/fetchModelCard.ts
@@ -0,0 +1,5 @@
+import { ModelCard } from '../tugboat/lib/ModelCard';
+
+export function fetchModelCard(id: string): Promise<ModelCard> {
+    return fetch(`/api/model-card/${id}`).then(r => r.json())
+}
