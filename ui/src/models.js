78d42848abae80b4dcdb80c6ad6f9ca51dbcbe93
jonathan m borchardt <jonborchardt@users.noreply.github.com>
2021-02-01 11:30:43 -0800

    Jon delete (#790)

--- ui/src/models.js
+++ ui/src/models.js
@@ -1,62 +1,19 @@
-import TextualEntailment from './components/demos/TextualEntailment';
-import SentimentAnalysis from './components/demos/SentimentAnalysis';
-import VisualQuestionAnswering from './components/demos/VisualQuestionAnswering';
 import LanguageModel from './components/demos/LanguageModel';
-import MaskedLm from './components/demos/MaskedLm';
-import annotateIcon from './icons/annotate-14px.svg';
 import otherIcon from './icons/other-14px.svg';
-import passageIcon from './icons/passage-14px.svg';
-import questionIcon from './icons/question-14px.svg';
 
 // This is the order in which they will appear in the menu
 const modelGroups = [
-    {
-        label: 'Answer a question',
-        iconSrc: questionIcon,
-        defaultOpen: true,
-        models: [
-            {
-                model: 'visual-question-answering',
-                name: 'Visual Question Answering',
-                component: VisualQuestionAnswering,
-            },
-        ],
-    },
-    {
-        label: 'Annotate a sentence',
-        iconSrc: annotateIcon,
-        defaultOpen: true,
-        models: [
-            {
-                model: 'sentiment-analysis',
-                name: 'Sentiment Analysis',
-                component: SentimentAnalysis,
-            },
-        ],
-    },
-    {
-        label: 'Annotate a passage',
-        iconSrc: passageIcon,
-        defaultOpen: true,
-        models: [],
-    },
     {
         label: 'Other',
         iconSrc: otherIcon,
         defaultOpen: true,
         models: [
             {
-                model: 'textual-entailment',
-                name: 'Textual Entailment',
-                component: TextualEntailment,
-            },
-            {
                 model: 'next-token-lm',
                 name: 'Language Modeling',
                 component: LanguageModel,
                 redirects: ['gpt2'],
             },
-            { model: 'masked-lm', name: 'Masked Language Modeling', component: MaskedLm },
         ],
     },
 ];
