4fa702933af8ff0159b2890d1adf8f8947fe1a1d
Sam Skjonsberg <sams@allenai.org>
2021-02-18 14:16:28 -0800

    Fetch info model by model.
    This modifies the front-end to query info for each model on a
    per-model basis. This is probably a bit slower, but also means
    that when running the stack locally the info is provided by
    the local version that's running instead of production.
    
    If we find it's too slower for users we can probably find other
    ways to speed things up.
    
    I also removed the aggregate `/api/info` route. Nothing uses it
    after this change, and it involves all sorts of complicated
    machinery, so it felt ok to remove.

--- ui/src/components/TaskDemo.tsx
+++ ui/src/components/TaskDemo.tsx
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Model, Task, Example } from '@allenai/tugboat/lib';
-import { TaskDemo as TBTaskDemo } from '@allenai/tugboat/components';
+import { TaskDemo as TBTaskDemo, Promised } from '@allenai/tugboat/components';
 
 import { AppId } from '../AppId';
-import { TaskCard, getModelCardId, ModelInfo } from '../lib';
+import { fetchModelInfo, TaskCard, getModelCardId, ModelInfo } from '../lib';
 import { ModelCards, ModelInfoList, TaskCards } from '../context';
 import { RedirectLegacyPermalinks } from '../components';
 
@@ -46,44 +46,40 @@ interface Props {
  * of a model (which is queried via API routes) to the shape expected by the tugboat module.
  */
 export const TaskDemo = ({ ids, taskId, children, examples }: Props) => {
-    const infos = useContext(ModelInfoList);
-    const included = new Set(ids);
-    const demoInfos = infos.filter((info) => included.has(info.id));
-    const missingIds = ids.filter((id) => !demoInfos.find((mi) => mi.id === id));
-    if (missingIds.length > 0) {
-        console.log(
-            "The following models were requested but skipped because they weren't found in the " +
-                "info response. This usually means the API for that model is down, or that there's " +
-                'a typo: ',
-            missingIds
-        );
-    }
-
-    const tasksById = useContext(TaskCards);
-    if (!(taskId in tasksById)) {
-        throw new TaskNotFoundError(taskId);
-    }
-    const task = tasksById[taskId];
-
-    const cardsById = useContext(ModelCards);
-    const models: Model[] = [];
-    for (const info of demoInfos) {
-        const modelCardId = getModelCardId(info);
-        if (!(modelCardId in cardsById)) {
-            throw new ModelCardNotFoundError(info);
-        }
-        models.push(new Model(info.id, cardsById[modelCardId]));
-    }
-
+    const fetchInfoForIncludedModels = () => Promise.all(ids.map(fetchModelInfo));
     return (
-        <RedirectLegacyPermalinks modelIds={ids}>
-            <TBTaskDemo
-                models={models}
-                task={asTugBoatTask(task)}
-                appId={AppId}
-                examples={examples}>
-                {children}
-            </TBTaskDemo>
-        </RedirectLegacyPermalinks>
+        <Promised promise={fetchInfoForIncludedModels} deps={ids}>{
+            (infos) => {
+                const tasksById = useContext(TaskCards);
+                if (!(taskId in tasksById)) {
+                    throw new TaskNotFoundError(taskId);
+                }
+                const task = tasksById[taskId];
+
+                const cardsById = useContext(ModelCards);
+                const models: Model[] = [];
+                for (const info of infos) {
+                    const modelCardId = getModelCardId(info);
+                    if (!(modelCardId in cardsById)) {
+                        throw new ModelCardNotFoundError(info);
+                    }
+                    models.push(new Model(info.id, cardsById[modelCardId]));
+                }
+                return (
+                    <ModelInfoList.Provider value={infos}>
+                        <RedirectLegacyPermalinks modelIds={ids}>
+                            <TBTaskDemo
+                                models={models}
+                                task={asTugBoatTask(task)}
+                                appId={AppId}
+                                examples={examples}>
+                                {children}
+                            </TBTaskDemo>
+                        </RedirectLegacyPermalinks>
+                    </ModelInfoList.Provider>
+                );
+
+            }
+        }</Promised>
     );
 };
