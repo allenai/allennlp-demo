1c1b3b3cf6c53f0f31771fb23c8ceda2fbd99f01
jon borchardt <jon.borchardt@gmail.com>
2021-03-29 15:09:14 -0700

    different logging

--- it/cypress/integration/rc_spec.ts
+++ it/cypress/integration/rc_spec.ts
@@ -1,56 +1,23 @@
 describe('Reading Comprehension', () => {
     it('is the default demo', () => {
-        const baseUrl = Cypress.config().baseUrl;
         cy.visit('/')
 
-        cy.url().then((val) => cy.log(`'${val}' should equal -> '${baseUrl}'`)); // TODO: remove
+        const baseUrl = Cypress.config().baseUrl;
         cy.url().should('equal', `${baseUrl}/reading-comprehension/bidaf-elmo`);
     });
 
     it('loads without an error', () => {
-        cy.get('h3').then((val) => {
-            const exp = 'Reading Comprehension';
-            console.log(`'${val[0].innerText}' should contain -> '${exp}'`); // TODO: remove
-            cy.contains(exp);
+        cy.document().get('body').then((val) => {
+            console.log(val[0].innerText); // TODO: remove
+            cy.log(val[0].innerText); // TODO: remove
         });
 
-        cy.get('p').then((val) => {
-            const exp = 'Reading comprehension is the task of answering questions about a passage of';
-            console.log(`'${val[0].innerText}' should contain -> '${exp}'`); // TODO: remove
-            cy.contains(exp);
-        });
-
-        cy.get('form').within(() => {
-            cy.get('b').then((val) => {
-                const exp = 'ELMo-BiDAF';
-                cy.log(`'${val[0].innerText}' should contain -> '${exp}'`); // TODO: remove
-                cy.contains(exp);
-            });
-
-            cy.get('p').then((val) => {
-                const exp = 'This is an implementation of the BiDAF model with ELMo embeddings. The';
-                cy.log(`'${val[1].innerText}' should contain -> '${exp}'`); // TODO: remove
-                cy.contains(exp);
-            })
-        });
-
-        cy.get('.ant-tabs').within(() => {
-            cy.get('.ant-select-selection-placeholder').then((val) => {
-                const exp = 'Select a Question';
-                cy.log(`'${val[0].innerText}' should contain -> '${exp}'`); // TODO: remove
-                cy.contains(exp);
-            });
-            cy.get('label').then((val) => {
-                const exp = 'Passage';
-                cy.log(`'${val[1].innerText}' should contain -> '${exp}'`); // TODO: remove
-                cy.contains(exp);
-            });
-
-            cy.get('label').then((val) => {
-                const exp = 'Question';
-                cy.log(`'${val[2].innerText}' should contain -> '${exp}'`); // TODO: remove
-                cy.contains(exp);
-            });
-        });
+        cy.contains('Reading Comprehension');
+        cy.contains('Reading comprehension is the task of answering questions about a passage of');
+        cy.contains('ELMo-BiDAF');
+        cy.contains('This is an implementation of the BiDAF model with ELMo embeddings. The');
+        cy.contains('Select a Question');
+        cy.contains('Passage');
+        cy.contains('Question');
     });
 });
